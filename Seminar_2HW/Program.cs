e870cb712f4aa294ea32db518d863b7f6b1799fe
Ilmira <gal.ilmir@mail.ru>
2022-04-23 23:12:41 +0300

    add Seminar_2 home work

---
+++ Seminar_2HW/Program.cs
@@ -0,0 +1,15 @@
+﻿// **Home Work**
+ // Напишите программу, которая принимает 
+ //на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+ void SecondNumber()
+ {
+     Console.WriteLine("ВВедите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    int num1 = num/10; 
+    int num2 = num1%10;
+    Console.WriteLine( " Вторая цифра " + num2);
+    
+ }
+ SecondNumber();
